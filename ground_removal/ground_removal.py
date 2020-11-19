342e2d07b2a6799668bd8b3412f89b798fc7c2c7
mk-a <adrien.mainka@gmail.com>
2020-11-19 09:49:28 -0500

    numba acceleration

--- ground_removal/ground_removal.py
+++ ground_removal/ground_removal.py
@@ -2,6 +2,17 @@ import laspy
 import numpy as np
 from pathlib import Path
 import argparse
+from numba import njit
+
+
+@njit
+def get_mask(pc, min_x, min_y, d_x, d_y, i, j):
+    return np.argwhere(
+        (pc[:, 0] >= min_x + i * d_x)
+        & (pc[:, 0] < min_x + (i + 1) * d_x)
+        & (pc[:, 1] >= min_y + j * d_y)
+        & (pc[:, 1] < min_y + (j + 1) * d_y)
+    ).flatten()
 
 
 def main():
@@ -13,25 +24,20 @@ def main():
     las = laspy.file.File(in_path, mode="r")
     pc = np.column_stack((las.x, las.y, las.z))
     min_x, min_y, min_z = pc.min(axis=0)
-    max_x, max_y, max_z = pc.max(axis=0) + 1e-3
+    max_x, max_y, max_z = pc.max(axis=0) + 1e-3  # add constant to include border points
+    # d_x and d_y defines the size of the subdivisions
+    # d_z is the difference betzeen the global max and min of height
     d_x, d_y, d_z = pc.max(axis=0) - pc.min(axis=0)
     d_x /= args.x
     d_y /= args.y
-    # ground = np.full(len(las), False)
     ground = np.full(len(las), args.c_n_steps, dtype=np.uint16)
-    # ground = np.ones(len(las), dtype=np.uint16)
     pt_src_id = np.zeros(len(las), dtype=np.uint16)
     n = 0
     c_values = np.linspace(args.c_max, args.c_min, args.c_n_steps)
     print(list(zip(range(args.c_n_steps - 1, -1, -1), c_values)))
     for i in range(args.x):
         for j in range(args.y):
-            mask = np.argwhere(
-                (pc[:, 0] >= min_x + i * d_x)
-                & (pc[:, 0] < min_x + (i + 1) * d_x)
-                & (pc[:, 1] >= min_y + j * d_y)
-                & (pc[:, 1] < min_y + (j + 1) * d_y)
-            ).flatten()
+            mask = get_mask(pc, min_x, min_y, d_x, d_y, i, j)
             if mask.size == 0:
                 continue
             print("{:03d} {:03d}".format(i, j), end="\r")
@@ -41,14 +47,8 @@ def main():
             C_list = []
             for c in c_values:
                 C_list.append(local_min + d_z * (1 - (max_z - local_max) / d_z) * c)
-            # print(C_list)
-            # print(list(enumerate(sorted(C_list)))[::-1])
-
-            # for k, C_he in list(enumerate(sorted(C_list)))[::-1]:
             for k, C_he in zip(range(9, -1, -1), C_list):
                 ground[mask[local_pc[:, 2] <= C_he]] = k
-            # C_he = local_min + d_z * (1 - (max_z - local_max) / d_z) * args.c
-            # ground[mask[local_pc[:, 2] <= C_he]] = 2
             pt_src_id[mask] = n
             n += 1
     las2 = laspy.file.File(out_path, mode="w", header=las.header)
